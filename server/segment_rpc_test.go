d2524f948bcf8a6097e4ddd9b716e7d99c6e8e4d
Yondon Fu <yondon.fu@gmail.com>
2019-02-26 18:31:09 -0500

    server: SubmitSegment unit tests

--- server/segment_rpc_test.go
+++ server/segment_rpc_test.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"bytes"
+	"crypto/tls"
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 
@@ -18,6 +20,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/http2"
 )
 
 func TestServeSegment_GetPaymentError(t *testing.T) {
@@ -333,4 +336,181 @@ func TestServeSegment_ReturnMultipleTranscodedSegmentData(t *testing.T) {
 	assert.True(ok)
 	assert.Equal([]byte("foo"), res.Data.Sig)
 	assert.Equal(2, len(res.Data.Segments))
+}
+
+func TestSubmitSegment_GenSegCredsError(t *testing.T) {
+	b := StubBroadcaster2()
+	b.signErr = errors.New("Sign error")
+
+	s := &BroadcastSession{
+		Broadcaster: b,
+		ManifestID:  core.RandomManifestID(),
+	}
+
+	_, err := SubmitSegment(s, &stream.HLSSegment{}, 0)
+
+	assert.Equal(t, "Sign error", err.Error())
+}
+
+func TestSubmitSegment_HttpPostError(t *testing.T) {
+	s := &BroadcastSession{
+		Broadcaster: StubBroadcaster2(),
+		ManifestID:  core.RandomManifestID(),
+		OrchestratorInfo: &net.OrchestratorInfo{
+			Transcoder: "https://foo.com",
+		},
+	}
+
+	_, err := SubmitSegment(s, &stream.HLSSegment{}, 0)
+
+	assert.Contains(t, err.Error(), "connection refused")
+}
+
+func TestSubmitSegment_Non200StatusCode(t *testing.T) {
+	ts, mux := stubTLSServer()
+	defer ts.Close()
+	mux.HandleFunc("/segment", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Server error", http.StatusInternalServerError)
+	})
+
+	s := &BroadcastSession{
+		Broadcaster: StubBroadcaster2(),
+		ManifestID:  core.RandomManifestID(),
+		OrchestratorInfo: &net.OrchestratorInfo{
+			Transcoder: ts.URL,
+		},
+	}
+
+	_, err := SubmitSegment(s, &stream.HLSSegment{}, 0)
+
+	assert.Equal(t, "Server error", err.Error())
+}
+
+func TestSubmitSegment_ProtoUnmarshalError(t *testing.T) {
+	ts, mux := stubTLSServer()
+	defer ts.Close()
+	mux.HandleFunc("/segment", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("foo"))
+	})
+
+	s := &BroadcastSession{
+		Broadcaster: StubBroadcaster2(),
+		ManifestID:  core.RandomManifestID(),
+		OrchestratorInfo: &net.OrchestratorInfo{
+			Transcoder: ts.URL,
+		},
+	}
+
+	_, err := SubmitSegment(s, &stream.HLSSegment{}, 0)
+
+	assert.Contains(t, err.Error(), "proto")
+}
+
+func TestSubmitSegment_TranscodeResultError(t *testing.T) {
+	tr := &net.TranscodeResult{
+		Result: &net.TranscodeResult_Error{Error: "TranscodeResult error"},
+	}
+	buf, err := proto.Marshal(tr)
+	require.Nil(t, err)
+
+	ts, mux := stubTLSServer()
+	defer ts.Close()
+	mux.HandleFunc("/segment", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write(buf)
+	})
+
+	s := &BroadcastSession{
+		Broadcaster: StubBroadcaster2(),
+		ManifestID:  core.RandomManifestID(),
+		OrchestratorInfo: &net.OrchestratorInfo{
+			Transcoder: ts.URL,
+		},
+	}
+
+	_, err = SubmitSegment(s, &stream.HLSSegment{}, 0)
+
+	assert.Equal(t, "TranscodeResult error", err.Error())
+}
+
+func TestSubmitSegment_Success(t *testing.T) {
+	require := require.New(t)
+
+	tr := &net.TranscodeResult{
+		Result: &net.TranscodeResult_Data{
+			Data: &net.TranscodeData{
+				Segments: []*net.TranscodedSegmentData{
+					&net.TranscodedSegmentData{Url: "foo"},
+				},
+				Sig: []byte("bar"),
+			},
+		},
+	}
+	buf, err := proto.Marshal(tr)
+	require.Nil(err)
+
+	var runChecks func(r *http.Request)
+
+	ts, mux := stubTLSServer()
+	defer ts.Close()
+	mux.HandleFunc("/segment", func(w http.ResponseWriter, r *http.Request) {
+		if runChecks != nil {
+			runChecks(r)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(buf)
+	})
+
+	s := &BroadcastSession{
+		Broadcaster: StubBroadcaster2(),
+		ManifestID:  core.RandomManifestID(),
+		OrchestratorInfo: &net.OrchestratorInfo{
+			Transcoder: ts.URL,
+		},
+	}
+
+	assert := assert.New(t)
+
+	runChecks = func(r *http.Request) {
+		assert.Equal("video/MP2T", r.Header.Get("Content-Type"))
+
+		data, err := ioutil.ReadAll(r.Body)
+		require.Nil(err)
+
+		assert.Equal([]byte("dummy"), data)
+	}
+
+	tdata, err := SubmitSegment(s, &stream.HLSSegment{Data: []byte("dummy")}, 0)
+
+	assert.Nil(err)
+	assert.Equal(1, len(tdata.Segments))
+	assert.Equal("foo", tdata.Segments[0].Url)
+	assert.Equal([]byte("bar"), tdata.Sig)
+
+	// Test when input data is uploaded
+	runChecks = func(r *http.Request) {
+		assert.Equal("application/vnd+livepeer.uri", r.Header.Get("Content-Type"))
+
+		data, err := ioutil.ReadAll(r.Body)
+		require.Nil(err)
+
+		assert.Equal([]byte("foo"), data)
+	}
+
+	SubmitSegment(s, &stream.HLSSegment{Name: "foo", Data: []byte("dummy")}, 0)
+}
+
+func stubTLSServer() (*httptest.Server, *http.ServeMux) {
+	mux := http.NewServeMux()
+
+	ts := httptest.NewUnstartedServer(mux)
+	ts.TLS = &tls.Config{
+		// This config option fixes the "unexpected ALPN protocol ""; want h2" error
+		NextProtos: []string{http2.NextProtoTLS},
+	}
+	ts.StartTLS()
+
+	return ts, mux
 }
